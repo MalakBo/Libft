ba25e20b8965f4e67f6c22eb2b628f03c9bcae91
MalakBo <malakbouyi8@gmail.com>
2024-11-03 22:28:48 +0100

    hhh

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: mbouyi <mbouyi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 20:27:42 by mbouyi            #+#    #+#             */
-/*   Updated: 2024/10/28 22:42:44 by mbouyi           ###   ########.fr       */
+/*   Updated: 2024/11/03 22:28:26 by mbouyi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ static int	ft_count_digits(int number)
 	int	count;
 
 	count = 0;
+	if(number = 0)
+		return 1;
 	while (number)
 	{
 		number = number / 10;
@@ -42,6 +44,11 @@ char	*ft_itoa(int n)
 	if (!s)
 		return (NULL);
 	s[len] = '\0';
+	if(n == 0)
+	{
+		s[0] = '0';
+		return s;
+	}
 	while (len--)
 	{
 		s[len] = (number % 10) + '0';
@@ -51,10 +58,10 @@ char	*ft_itoa(int n)
 		s[0] = '-';
 	return (s);
 }
-/*int main(){
+int main(){
 	int num;
-	num = -1005;
+	num = 0;
 	char *s = ft_itoa(num);
 	printf("%s\n",s);
 	return (0);
-}*/
+}
